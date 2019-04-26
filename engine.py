9ee98fc552d357dd866de6bc377df9121ae7677f
Yev Barkalov <yevbar1729@gmail.com>
2019-04-25 20:48:44 -0700

    add evaluation and minimax

--- engine.py
+++ engine.py
@@ -4,12 +4,111 @@ import time
 import random
 import chess
 
+def minimax(board):
+  depth = 1
+  chain = []
+
+  counter = 0
+  while counter < depth:
+    move_scores = []
+    for move in board.legal_moves:
+      board.push(move)
+      move_scores.append([move, evaluate(board)])
+      board.pop()
+    move_scores  = sorted(move_scores, key=lambda x: x[1], reverse=(counter%2!=0))
+    print("here are the move_scores")
+    for a_move in move_scores:
+      print(a_move[1])
+    print("we are done printing move_scores")
+    best = move_scores[0]
+    # print(best)
+    chain.append(move_scores[0])
+    counter += 1
+
+  chain.sort(key=lambda x: x[1])
+  return chain[0][0]
+      
+def evaluate(board):
+  if board is None:
+    return -1000
+  score = 0
+  _PAWN = 1
+  _KNIGHT = 3
+  _BISHOP = 3
+  _ROOK = 5
+  _QUEEN = 9
+  _KING = 100
+
+  turn = board.turn
+
+  for pawn in board.pieces(chess.PAWN, turn):
+    #print(pawn)
+    score += _PAWN
+
+  for knight in board.pieces(chess.KNIGHT, turn):
+    #print(knight)
+    score += _KNIGHT
+
+  for bishop in board.pieces(chess.BISHOP, turn):
+    #print(bishop)
+    score += _BISHOP
+
+  for rook in board.pieces(chess.ROOK, turn):
+    #print(rook)
+    score += _ROOK
+
+  for queen in board.pieces(chess.QUEEN, turn):
+    #print(queen)
+    score += _QUEEN
+  
+  for king in board.pieces(chess.KING, turn):
+    #print(king)
+    score += _KING
+
+  # the other side
+  for pawn in board.pieces(chess.PAWN, (not turn)):
+    #print(pawn)
+    score -= _PAWN
+
+  for knight in board.pieces(chess.KNIGHT, (not turn)):
+    #print(knight)
+    score -= _KNIGHT
+
+  for bishop in board.pieces(chess.BISHOP, (not turn)):
+    #print(bishop)
+    score -= _BISHOP
+
+  for rook in board.pieces(chess.ROOK, (not turn)):
+    #print(rook)
+    score -= _ROOK
+
+  for queen in board.pieces(chess.QUEEN, (not turn)):
+    #print(queen)
+    score -= _QUEEN
+  
+  for king in board.pieces(chess.KING, (not turn)):
+    #print(king)
+    score -= _KING
+
+  return score
+
 def get_move(board, limit=None):
   # TODO: Fill this in with an actual chess engine
-  move = random.choice(list(board.legal_moves))
-
+  output = None
+  
+  moves = list(board.legal_moves)
+  print("We are checking out the pieces at hand")
+  # print(board.piece_map)
+  # for move in moves:
+    # print(move)
+  print("The score is")
+  # print(evaluate(board))
+  minimax(board)
+    
   #print("playing", move, file=sys.stderr)
-  return move
+  # output = random.choice(list(board.legal_moves))
+  output = minimax(board)
+  return output
 
 if __name__ == "__main__":
   while 1:
