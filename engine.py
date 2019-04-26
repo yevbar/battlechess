75b7267c1b8b4f85ad22ddab9a8665c706426445
Yev Barkalov <yevbar1729@gmail.com>
2019-04-25 21:24:34 -0700

    just fucking around

--- engine.py
+++ engine.py
@@ -4,8 +4,31 @@ import time
 import random
 import chess
 
+def recursive_minimax(board, depth, my_turn):
+  if depth == 0 or board.is_checkmate():
+    return [board, evaluate(board)]
+
+  if my_turn:
+    winner = [None, -1000]
+    for move in board.legal_moves:
+      new_board = board
+      new_board.push(move)
+      mm = recursive_minimax(new_board, depth-1, False)
+      if mm[1] > winner[1]:
+        winner = mm
+    return winner
+  else: # not my_turn
+    winner = [None, 1000]
+    for move in board.legal_moves:
+      new_board = board
+      new_board.push(move)
+      mm = recursive_minimax(new_board, depth-1, True)
+      if mm[1] < winner[1]:
+        winner = mm
+    return winner
+
 def minimax(board):
-  depth = 1
+  depth = 3
   chain = []
 
   counter = 0
@@ -18,13 +41,16 @@ def minimax(board):
     move_scores  = sorted(move_scores, key=lambda x: x[1], reverse=(counter%2!=0))
     print("here are the move_scores")
     for a_move in move_scores:
-      print(a_move[1])
+      print(a_move)
     print("we are done printing move_scores")
     best = move_scores[0]
     # print(best)
     chain.append(move_scores[0])
     counter += 1
 
+  print("this is what the current chain looks like")
+  for thing in chain:
+    print(thing)
   chain.sort(key=lambda x: x[1])
   return chain[0][0]
       
@@ -107,8 +133,13 @@ def get_move(board, limit=None):
     
   #print("playing", move, file=sys.stderr)
   # output = random.choice(list(board.legal_moves))
-  output = minimax(board)
-  return output
+  # output = minimax(board)
+  # minimax winner
+  mm_winner = recursive_minimax(board, 1, True)
+  last_move = None
+  while board != mm_winner:
+    last_move = mm_winner.pop()
+  return last_move
 
 if __name__ == "__main__":
   while 1:
